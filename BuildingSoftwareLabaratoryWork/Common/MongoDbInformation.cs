119c39f7c454b2bffc3423647160687d4422774a
xdzz-cyber <bogdankrut12@gmail.com>
2022-10-25 21:38:23 +0300

    remove config info

--- BuildingSoftwareLabaratoryWork/Common/MongoDbInformation.cs
+++ BuildingSoftwareLabaratoryWork/Common/MongoDbInformation.cs
@@ -5,5 +5,5 @@ namespace BuildingSoftwareLabaratoryWork.Common;
 public static class MongoDbInformation
 {
     public static MongoClient GetClient()
-        => new("mongodb+srv://BHbrk:12345@cluster0.gtmed.azure.mongodb.net/?retryWrites=true&w=majority");
+        => new();
 }
